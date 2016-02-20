5dcef452ba7b4c89e66ad0d3194a42c3a5523a47
Amer Almadani <mad_dev@linuxmail.org>
2016-02-20 23:46:34 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='HashLink',
+      version='1.0',
+      description='Random stuff daily',
+      author='Amer Almadani',
+      author_email='mad_dev@linuxmail.org',
+      url='http://x.datasig.io',
+     install_requires=['Flask>=0.10.1', 'urllib3==1.14', 'feedparser==5.2.1','beautifulsoup4==4.4.1'],
+     )
